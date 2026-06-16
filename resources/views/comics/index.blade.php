858a58f2db2cdd7f521b433024435569c6a552b8
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-16 22:18:08 +0200

    create table with comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,20 +3,26 @@
 @section('content')
     <section id="comics">
         <div class="container mt-5">
-            <div class="row row-cols-5">
-                @foreach ($comics as $comic)
-                    <div class="col">
-                        <div class="card h-100">
-                            <img src={{asset('storage/' . $comic->cover_img)}} class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">
-                                    {{$comic->title}}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Pubblicazione</th>
+                        <th scope="col">Prezzo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                    <tr>
+                        <th scope="row">{{$comic->id}}</th>
+                        <td>{{$comic->title}}</td>
+                        <td>{{$comic->release_date}}</td>
+                        <td>{{$comic->price}} . &euro;</td>  
+                    </tr>
+                        @endforeach
+                </tbody>
+            </table>
         </div>
     </section>
 @endsection
