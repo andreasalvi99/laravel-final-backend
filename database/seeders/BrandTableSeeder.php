ee190dd278d58e31d6ba75788c4a346300af787f
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-16 20:10:44 +0200

    run seed for DC and Marvel

--- database/seeders/BrandTableSeeder.php
+++ database/seeders/BrandTableSeeder.php
@@ -15,8 +15,11 @@ class BrandTableSeeder extends Seeder
     {
         $newBrand = new Brand();
 
-        $newBrand->name = 'DC Comics';
-        $newBrand->description = 'DC Comics è una delle principali case editrici americane di fumetti, conosciuta per personaggi leggendari come Batman, Superman, Wonder Woman e per universi narrativi che hanno influenzato generazioni di lettori.';
+        $newBrand->name = 'Marvel Comics';
+        $newBrand->description = 'Marvel Comics è una storica casa editrice statunitense di fumetti fondata nel 1939, conosciuta in tutto il mondo per aver creato alcuni dei personaggi più celebri della cultura pop, tra cui Spider-Man, Iron Man, Captain America e gli Avengers.';
+        $newBrand->logo = 'brands/marvel-logo.svg';
+
+        $newBrand->save();
 
     }
 }
