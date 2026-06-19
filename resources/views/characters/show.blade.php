0a2443ae464d4798d9428ba3319298e634ea91a3
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-19 16:24:09 +0200

    add link

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -3,6 +3,11 @@
 @section('content')
     <section>
         <div class="container mt-5">
+            <div class="mb-3">
+                <a href="{{route('characters.index')}}" class="text-dark">
+                    <i class="bi bi-arrow-left me-2"></i>Torna  ai personaggi
+                </a>
+            </div>
             <div class="card mb-3">
                 <div class="row g-0">
                     <div class="col-md-4">
