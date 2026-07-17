3624eaa4d69c35f3809a15226d4c1c2f01bc3f86
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-07-17 12:45:03 +0200

    fix deploy issues

--- config/database.php
+++ config/database.php
@@ -54,9 +54,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? [
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ] : [],
+            'options' => extension_loaded('pdo_mysql') ? [] : [],
         ],
 
         'mariadb' => [
