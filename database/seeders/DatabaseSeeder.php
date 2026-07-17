8f0f1f7cb2586eee97b160c810f4e9871b535cd9
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-07-17 13:38:11 +0200

    fix charset issues

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+         $this->call([
+        BrandTableSeeder::class,
+        ComicTableSeeder::class,
+    ]);
     }
 }
