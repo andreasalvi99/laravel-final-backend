861adae7edcb30aab643c00655c3c7ebf0f87b7a
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-07-25 11:15:45 +0200

    remove debug lines

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-<h1>TEST COMICS</h1>
 
 @section('content')
     <section id="comics">
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Admin\ComicController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/comics', function () {
-    return 'FUNZIONA COMICS';
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -24,14 +20,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/comics-test', function () {
-    return 'COMICS ROUTE OK';
-});
-
-Route::get('/comics', function () {
-    return 'COMICS INDEX TEST';
-});
-
 Route::resource('comics', ComicController::class)
     ->middleware(['auth', 'verified']);
 
@@ -40,9 +28,5 @@ Route::resource('brands', BrandController::class)
 
 Route::resource('characters', CharacterController::class)
     ->middleware(['auth', 'verified']);
-
-    Route::get('/test', function () {
-    return 'OK';
-});
 
 require __DIR__.'/auth.php';
