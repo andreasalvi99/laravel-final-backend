8ed64a83959fd10fbd10723b9b606da017ccae3f
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-09-16 14:50:57 +0200

    create migration for orders table

---
+++ database/migrations/2026_09_16_123450_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('address_type');
+            $table->string('address');
+            $table->string('city');
+            $table->string('house_number');
+            $table->string('province');
+            $table->string('zipcode');
+            $table->decimal('total', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
