401139909f847789dffd6a5989991d70aed7e323
Andrea Salvi <andrea.salvi.r@gmail.com>
2026-09-21 18:29:55 +0200

    add key in order controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -25,6 +25,7 @@ class OrderController extends Controller
         $newOrder->province = $data['province'];
         $newOrder->zipcode = $data['zipcode'];
         $newOrder->total = $data['total'];
+        $newOrder->shipping_method['shipping_method'];
 
         $newOrder->save();
 
